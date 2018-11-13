def8690be9152d919ec2a9210e0fd87f020202f0
janehuang1647 <jane1647@hotmail.com>
2018-11-13 10:34:28 -0800

    version 1

--- cm107/bcl/app.R
+++ cm107/bcl/app.R
@@ -9,15 +9,39 @@
 
 library(shiny)
 
+a <- 5
+print(a^2)
+
+
+bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
-  
-)
+  "this is some texts",
+  br(),
+  p("this is some texts"),
+  tags$h1(em("level 1 header")),
+  h1("test"),
+  HTML("<H1>Level 1 header, part 3</h1>"),
+
+  br(),
+  p("what am i?"),
+a(href="http://stat545.com/Classroom/notes/cm107.nb.html#why-use-shiny","link to main page!"),
+print(a),
+br(),
+
+
+titlePanel("BC Liquor price app", 
+           windowTitle = "BCL app"),
+sidebarLayout(
+  sidebarPanel("This text is in the sidebar."),
+  mainPanel(
+    plotOutput("price_hist"))
+))
+ 
 # Define server logic required to draw a histogram
 server <- function (input,output){
-  
-}
+  output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
+} 
 
 # Run the application 
 shinyApp(ui = ui, server = server)
